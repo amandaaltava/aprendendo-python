0d6dbd0d7a01c14c1a9b7cf299eeea16e0fddaaa
amandaaltava <a.amanda.altava@hotmail.com>
2022-10-11 20:02:18 -0300

    programa com for que le 6 numeros e diz se é par ou ímpar

--- guanabara/aula13ex50.py
+++ guanabara/aula13ex50.py
@@ -1,3 +1,18 @@
 '''Faça um programa que leia 6 números inteiros
 e mostre a soma apenas daqueles que forem pares
 se o valor digitado for ímpar, desconsidere-o'''
+
+# seta o contador em 0
+soma = 0
+
+# seta contador em 0
+cont = 0
+
+# laço para pegar os números, conferir e somar
+for pares in range(1, 7):
+    n = int(input(f'Digite o {pares} número: '))
+    if n % 2 == 0:
+        soma += n # acumula os numeros que sao verdadeiros no laço
+        cont += 1  # conta quantos numeros são verdadeiros no laço
+
+print(f'Você informou {cont} e a soma dos números pares é {soma} .')
