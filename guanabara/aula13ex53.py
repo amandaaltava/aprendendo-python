e9f885e70bcc3e9a5ab284570832b61300a68b94
amandaaltava <a.amanda.altava@hotmail.com>
2022-10-11 20:59:59 -0300

    programa que lê uma frase e diz se é ou não palíndromo

--- guanabara/aula13ex53.py
+++ guanabara/aula13ex53.py
@@ -1,5 +1,5 @@
 '''Faça um programa que leia uma frase qualquer e diga se
-ela é um palíndromo descnsiderando os espaços
+ela é um palíndromo desconsiderando os espaços
 
 exemp palíndromos:
 apos a sopa
@@ -7,3 +7,25 @@ a sacada da casa
 a torre da derrota
 o lobo ama o bolo
 anotaram a data da maratona'''
+
+frase = str(input('Digite uma frase: ')).strip() .upper() #tira o espaços da frente e do final da palavra e joga tudo em maisucula  para poder comprar
+palavras = frase.split() #separa em palavras individuais
+junto = ''.join(palavras) #junta tudo sem espaços
+inverso = ''
+for letra in range(len(junto) -1, -1, -1):# -1(para começar a contar da última letra p 1°), -1(começa a contar do 0 ) -1(vai voltando letra por letra) VAI FAZER A PALAVRA DE TRAS PARA FRENTE
+       inverso += junto[letra]# monta e mostra a palavra ao contrario
+if inverso == junto:
+    print('Temos um palídromo')
+else:
+    print('Não é palíndromo')
+
+'''
+frase = str(input('frase')) .strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = junto[::-1] # joga a palavra do inicio para o final e de tras para frente
+if inverso == junto:
+    print('Palíndromo')
+else:
+    print('Não palíndromo')
+'''
