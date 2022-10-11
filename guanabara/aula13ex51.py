b115460ba627a4da4f6a41a6098cdf0489f2bbc6
amandaaltava <a.amanda.altava@hotmail.com>
2022-10-11 20:59:07 -0300

    programa que lê um número inteiro e diz se é ou não um número primo

--- guanabara/aula13ex51.py
+++ guanabara/aula13ex51.py
@@ -3,3 +3,14 @@ a razão de uma PROGRESSAO ARITIMÉTICA
  no final mostre os 10 PRIMEIROS TERMOS dessa progressao
 
  obs: pulando de tantos em tantos'''
+
+pt = int(input('Digite o primeiro termo: '))
+r = int(input('Digite a razão: '))
+decimo = pt + (10 - 1) * r    # calculo do nonagéssimo elemento da pa
+for c in range(pt, decimo + r, r):
+    print(f'{c}', end='>')
+print('Acabou')
+
+
+
+
--- guanabara/aula13ex52.py
+++ guanabara/aula13ex52.py
@@ -2,3 +2,20 @@
 e diga se ele é ou não um número primo
 
 obs: é auqele divisível por 1 e por ele mesmo e nenhum outro até ele'''
+
+n = int(input('Insira o número: '))
+c = 0
+
+for primo in range(1, n + 1):   # laço começa de 1 e adiciona 1 ao número
+    if n % primo == 0:       # verifica se ele é primo ou não dividindo o número pelo laço
+        print('\033[33m', end=' ')   # formata a cor para que o número do laço, receba a cor de acordo com o resultado do IF, ser positivo ou negativo
+        c += 1   # contador
+    else:
+        print('\033[31m', end=' ')
+    print(f'{primo}', end=' ')   # imprime o intervalo do laço que parte de 1 até o número e é o IF que
+
+print(f'O número {n} foi divisível {c} vezes.')
+if c == 2:       # se o contador, que é o número de vezes que ele foi divisível for até 2, então é primo
+    print('E por isso ele não é primo.')
+else:
+    print('E por isso ele não é primo.')
