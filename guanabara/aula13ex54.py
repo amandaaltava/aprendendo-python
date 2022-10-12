8b75f2cccc1cd2a1c0df701682d5c47285dc6a48
amandaaltava <a.amanda.altava@hotmail.com>
2022-10-11 21:01:10 -0300

    lê o ano de nascimento de sete pessoas e diz quantas já atingiram a maioridade

--- guanabara/aula13ex54.py
+++ guanabara/aula13ex54.py
@@ -1,3 +1,17 @@
 '''Faça um programa que  leia o ano de nascimento de sete pessoas
 e no final mostre quantas pessoas atingiram a maturidade, 21 anos
 e quantas não atingiram ainda a maturidade'''
+from datetime import date
+
+maior = 0 # inicia o somador da maioridade
+menor = 0 # inicia o somador da menor idade
+for laco in range(0, 7):              # laço
+    ano = int(input(f'Em que ano a pessoa {laco +1}º nasceu?: '))
+    ano = date.today().year - ano  # calculo da maioridade
+    if ano >= 21:    # condição para maioridade
+        maior = maior + 1
+    else:        # condição para a menor idado
+        menor = menor + 1
+
+print(f'{maior} pessoas atingiram a maioridade.')
+print(f'{menor} pessoas não atingiram a maioridade.')
