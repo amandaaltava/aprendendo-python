c89e403f8d183aabc41caf5b1c4be037d6ff73c1
amandaaltava <a.amanda.altava@hotmail.com>
2022-10-14 16:59:09 -0300

    programa que leia o nome, sexo e idade de 4 pessoas e diga qual o nome do homem mais velho, quantas mulheres abaixo de 20 anos e qual a media de idade do grupo.

--- guanabara/aula13ex56.py
+++ guanabara/aula13ex56.py
@@ -3,4 +3,52 @@ nome, idade e sexo de 4 pessoas
 no final do programa mostre:
 - a media de idade do grupo
 qual é o nome do homem mais velho
-quantas mulheres tem menos de 21 anos.'''
+quantas mulheres tem menos de 20 anos.'''
+import statistics
+from statistics import mean
+'''cont1 = 0
+nomeM = 0
+
+for laco in range(1, 5):
+    nome = str(input(f'Digite o nome da {laco}º pessoa: ').upper())
+    idade = int(input('Digite a idade: '))
+    sexo = str(input('Digite F ou M para FEMININO ou MASCULINO: ').upper())
+
+if sexo == 'F' and idade < 21:
+    cont1 = cont1
+
+if sexo == 'M':
+    nomeM = max(idade)
+
+idadeM = statistics.mean(idade)
+
+print(f'O homem mais velho se chama {nomeM}.')
+print(f'A média de idade do grupo é {idadeM}.')
+print(f'{cont1} mulheres tem menos de 21 anos.')'''
+
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
+
+for p in range(1, 5):
+    print(f'...{p}°...')
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]:')).strip()
+    somaidade = somaidade + idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 = totmulher20 + 1
+
+
+mediaidade = somaidade / 4
+print(f'A média de idade do grupo é de {mediaidade} anos.')
+print(f'O homem  mais velho se chama {nomevelho}, e tem {maioridadehomem} anos.')
+print(f'Ao todo são {totmulher20} mulheres com menos de 20 anos.')
---
+++ guanabara/teste.py
@@ -0,0 +1,6 @@
+soma = 0
+contador = 0
+for laco in range(0, 6):
+    soma = soma + laco
+    contador = contador + 1
+    print(contador, soma)
